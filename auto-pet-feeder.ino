b43ad3c648c24bb8a477f09d664e85d6d6103bb2
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-07-11 21:58:19 -0500

    Add the Arduino file

--- auto-pet-feeder.ino
+++ auto-pet-feeder.ino
@@ -1,43 +1,10 @@
-#include <ESP8266WebServer.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-
-ESP8266WebServer server(80);
-
-void handleRoot() {
-  String htmlPage = String("<label for=\"ssid\">SSID</label>") +
-                           "<select id=\"ssid\">";
-
-  for (int i = 0; i < WiFi.scanNetworks(); ++i) {
-    htmlPage += "<option>" + WiFi.SSID(i) + "</option>";
-  }
-
-  htmlPage += String("</select>") +
-              "<input type=\"text\" />";
-  
-  server.send(200, "text/html", htmlPage);
-
-  
-}
-
-void handleSpin() {
-  server.send(200, "text/html", "Spinning...");
-  
-  digitalWrite(5, HIGH);
-  delay(5000);
-  digitalWrite(5, LOW);
-}
-
 void setup() {
-  WiFi.softAP("Automatic Pet Feeder");
-  
-  server.on("/", handleRoot);
-  server.on("/spin", handleSpin);
-  server.begin();
-
-  pinMode(5, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  server.handleClient();
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
 }
