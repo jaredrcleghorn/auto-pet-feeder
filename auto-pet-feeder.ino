dd817396d76218d883fbeed11ff0b20cd6b18821
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-12-18 13:16:52 -0600

    Add network retrieval

--- auto-pet-feeder.ino
+++ auto-pet-feeder.ino
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 
@@ -7,7 +8,32 @@ void setup() {
 	// Set up a soft access point.
 	WiFi.softAP("Auto Pet Feeder");
 
-	server.on("/", [] { server.send(200, "text/plain", "hello, world"); });
+	server.on("/networks", [] {
+		int numNetworks = WiFi.scanNetworks();
+		DynamicJsonDocument doc(JSON_ARRAY_SIZE(numNetworks) + numNetworks * (JSON_OBJECT_SIZE(3) + 32));
+
+		for (int i = 0; i < numNetworks; ++i) {
+			JsonObject obj = doc.createNestedObject();
+
+			obj["name"] = WiFi.SSID(i);
+			obj["isEncrypted"] = WiFi.encryptionType(i) != ENC_TYPE_NONE;
+
+			const int rssi = WiFi.RSSI(i);
+			const int minRssi = -100;
+			const int maxRssi = -50;
+			const int numStrengths = 4;
+
+			if (rssi <= minRssi) {
+				obj["strength"] = 1;
+			} else if (rssi >= maxRssi) {
+				obj["strength"] = numStrengths;
+			} else {
+				obj["strength"] = numStrengths * (rssi - minRssi) / (maxRssi - minRssi) + 1;
+			}
+		}
+
+		server.send(200, "application/json", doc.as<String>());
+	});
 
 	// Start the server.
 	server.begin();
